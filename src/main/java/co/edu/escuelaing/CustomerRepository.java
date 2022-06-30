a41128b8a604f0cea7897aaf382d294723e41414
juanbaezl <juanbaez695@gmail.com>
2022-06-30 12:21:28 -0400

    first commit

---
+++ src/main/java/co/edu/escuelaing/CustomerRepository.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+
+    public List<Customer> findByLastName(String lastName);
+
+}
